29d2435cc8b3352c8d7f7ed2fdd93f34fd0be8b1
KingAli <a.tursunov1210@gmail.com>
2022-05-22 15:45:02 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/buttoncount_3_5/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.geektech.buttoncount_3_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private var textView: TextView? = null
+    private var btnPlus: Button? = null
+    private var sum = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.text_view)
+        btnPlus = findViewById(R.id.plus)
+        btnPlus?.setOnClickListener {
+            if (sum < 11 && btnPlus?.text == "+1") {
+                sum++
+                textView?.text = sum.toString()
+                if (sum == 10) {
+                    btnPlus?.text = "-1"
+                }
+            }
+        else {
+            sum --
+            textView?.text = sum.toString()
+            if (sum == 0 && btnPlus?.text == "-1"){
+                btnPlus?.text = "+1"
+            }
+        }
+        }
+    }
+    }
